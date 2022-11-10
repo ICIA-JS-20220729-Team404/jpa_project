620045c61f37fe964bb9d234e7fe28815324ee62
owohaha123 <104568039+owohaha123@users.noreply.github.com>
2022-11-10 10:29:26 +0900

    nf_community(setting)
    - 초기세팅

---
+++ nf_community/src/main/java/com/jsframe/nf_community/NfCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.jsframe.nf_community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NfCommunityApplication.class, args);
+    }
+
+}
