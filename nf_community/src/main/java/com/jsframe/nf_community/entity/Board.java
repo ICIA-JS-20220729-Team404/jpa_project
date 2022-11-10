33546b21c6fc7d2bfddd9811914d2fcae5ac8714
owohaha123 <104568039+owohaha123@users.noreply.github.com>
2022-11-10 11:10:05 +0900

    entity

---
+++ nf_community/src/main/java/com/jsframe/nf_community/entity/Board.java
@@ -0,0 +1,30 @@
+package com.jsframe.nf_community.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "boardtbl")
+@Data
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long bno;
+
+    @ManyToOne
+    @JoinColumn(name = "bid")
+    private Member bid;
+
+    @Column(nullable = false, length = 45)
+    private String btitle;
+
+    @Column(nullable = false)
+    private String bcontent;
+
+    @CreationTimestamp
+    @Column
+    private Timestamp bdate;
+}
