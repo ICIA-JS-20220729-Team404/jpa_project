181f62a4b2d1e22684a7696d9cdd8f3e96142c93
김윤주 <owohaha123@gmail.com>
2022-11-12 21:28:54 +0900

    로그인_회원가입

---
+++ nf_community/src/main/java/com/jsframe/nf_community/controller/MemberController.java
@@ -0,0 +1,48 @@
+package com.jsframe.nf_community.controller;
+
+import com.jsframe.nf_community.entity.Member;
+import com.jsframe.nf_community.service.MemberService;
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@Log
+public class MemberController {
+
+    @Autowired
+    private MemberService mServ;
+
+    @GetMapping("joinFrm")
+    public String joinFrm(){
+        log.info("joinFrm()");
+        return "joinFrm";
+    }
+
+    @PostMapping("joinProc")
+    public String joinProc(Member member, RedirectAttributes rttr){
+        log.info("joinProc()");
+        String view = mServ.joinProc(member, rttr);
+
+        return view;
+    }
+
+    @GetMapping("loginFrm")
+    public String loginFrm(){
+        log.info("loginFrm()");
+        return "loginFrm";
+    }
+
+    @PostMapping("loginProc")
+    public String loginProc(Member member, HttpSession session, RedirectAttributes rttr){
+        log.info("loginProc()");
+        String view = mServ.loginProc(member, session, rttr);
+
+        return view;
+    }
+}
---
+++ nf_community/src/main/java/com/jsframe/nf_community/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.jsframe.nf_community.repository;
+
+import com.jsframe.nf_community.entity.Member;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MemberRepository extends CrudRepository<Member, Long> {
+    Member findMemberByMid(String mid) ;
+    Member findMemberByMpw(String mpw) ;
+}
