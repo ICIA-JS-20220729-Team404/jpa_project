4497fe6c200d4d5b33c57349b6fb29ca592b5c48
ICIA20220729T5AR <ICIA20220729T5AR@gmail.com>
2022-11-14 23:10:57 +0900

    메인 컨트롤러 수정

--- nf_community/src/main/java/com/jsframe/nf_community/controller/MainController.java
+++ nf_community/src/main/java/com/jsframe/nf_community/controller/MainController.java
@@ -1,22 +1,31 @@
 package com.jsframe.nf_community.controller;
 
+import com.jsframe.nf_community.entity.Board;
+import com.jsframe.nf_community.service.BoardService;
 import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @Log
 public class MainController {
+    @Autowired
+    BoardService bServ;
 
     @GetMapping("/")
-    public String home(){
-        return "home";
+    public ModelAndView home(){
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("boardPage", bServ.getBoardPage(1));
+        mv.setViewName("home");
+        return mv;
     }
+
     @GetMapping("/loginEx.html")
     public String loginEx(){
         return "loginEx";
     }
-
-
-
 }
